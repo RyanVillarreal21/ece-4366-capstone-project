addf6d61fc73d2e5a96849ad4a2aa66f403b8dd9
Ryan Villarreal <Ryan.Villarreal@ttu.edu>
2021-11-18 09:11:37 -0600

    postfix working, adding andSym

--- combinational-logic-circuit-analysis.py
+++ combinational-logic-circuit-analysis.py
@@ -101,17 +101,18 @@ def checkExpress(expr):
             if(expr[i + 1] != ''):
                 expr = input("Invalid boolean expression. Please enter another: ")
                 checkExpress(expr)
-            else:
-                print("Evaluation success")
-                break
+            # else:
+            #     print("Evaluation success")
+            #     break
+    return expr
     # match = re.findall(r"[A-Z]", expr)
     # if match:
     #     return expr
     # else:
     #     expr = input("Invalid boolean expression. Please enter another: ")
-    #   checkExpress(expr)
+    #     checkExpress(expr)
 
-def evaluate(arr, x, y):
+#def evaluate(arr, x, y):
     # stack = []
     # n = len(arr)
     # l = x
@@ -149,94 +150,51 @@ def evaluate(arr, x, y):
     #         l = l + 1
     #         continue
 
-    # Constructor to initialize the class variables
-    def __init__(self, capacity):
-        self.top = -1
-        self.capacity = capacity
-        # This array is used a stack
-        self.array = []
-        # Precedence setting
-        self.output = []
-        self.precedence = {'+':1, '*':1, "'":2}
-     
-    # check if the stack is empty
-    def isEmpty(self):
-        return True if self.top == -1 else False
-     
-    # Return the value of the top of the stack
-    def peek(self):
-        return self.array[-1]
-     
-    # Pop the element from the stack
-    def pop(self):
-        if not self.isEmpty():
-            self.top -= 1
-            return self.array.pop()
+def andSym(expr):
+    n = len(expr)
+    i = 0
+    for i in range(0, n - 2, 2):
+        if(expr[i + 1] == "+" or "'" or "(" or ")"):
+            i += 1
+            continue
         else:
-            return "$"
-     
-    # Push the element to the stack
-    def push(self, op):
-        self.top += 1
-        self.array.append(op)
- 
-    # A utility function to check is the given character
-    # is operand
-    def isOperand(self, ch):
-        return ch.isalpha()
- 
-    # Check if the precedence of operator is strictly
-    # less than top of stack or not
-    def notGreater(self, i):
-        try:
-            a = self.precedence[i]
-            b = self.precedence[self.peek()]
-            return True if a  <= b else False
-        except KeyError:
-            return False
-             
-    # The main function that
-    # converts given infix expression
-    # to postfix expression
-    def infixToPostfix(self, exp):
-         
-        # Iterate over the expression for conversion
-        for i in exp:
-            # If the character is an operand,
-            # add it to output
-            if self.isOperand(i):
-                self.output.append(i)
-             
-            # If the character is an '(', push it to stack
-            elif i  == '(':
-                self.push(i)
- 
-            # If the scanned character is an ')', pop and
-            # output from the stack until and '(' is found
-            elif i == ')':
-                while( (not self.isEmpty()) and
-                                self.peek() != '('):
-                    a = self.pop()
-                    self.output.append(a)
-                if (not self.isEmpty() and self.peek() != '('):
-                    return -1
-                else:
-                    self.pop()
- 
-            # An operator is encountered
-            else:
-                while(not self.isEmpty() and self.notGreater(i)):
-                    self.output.append(self.pop())
-                self.push(i)
- 
-        # pop all the operator from the stack
-        while not self.isEmpty():
-            self.output.append(self.pop())
- 
-        print (self.output)
+            expr[i + 1].append("*")
+            i += 1
+            continue
+    return expr
+
+OPERATORS = set(['+', '*', "'", '(', ')'])  # set of operators
+PRIORITY = {'+':1, '*':1,"'":2} # dictionary having priorities 
+
+def infix_to_postfix(expr): #input expression
+    stack = [] # initially stack empty
+    output = '' # initially output empty
+
+    for ch in expr:
+        if(ch not in OPERATORS):  # if an operand then put it directly in postfix expression
+            output+= ch
+        elif(ch == '('):  # else operators should be put in stack
+            stack.append('(')
+        elif(ch == ')'):
+            while(stack and stack[-1] != '('):
+                output += stack.pop()
+            stack.pop()
+        else:
+            # lesser priority can't be on top on higher or equal priority    
+             # so pop and put in output   
+            while(stack and stack[-1] != '(' and PRIORITY[ch] <= PRIORITY[stack[-1]]):
+                output += stack.pop()
+            stack.append(ch)
+    while stack:
+        output += stack.pop()
+    return output
 
 expr = input("Enter a boolean expression: ")
 checkExpress(expr)
+andSym(expr)
+print('infix expression: ',expr)
+print('postfix expression: ',infix_to_postfix(expr))
+
 #arr = str.split(expr)
-evaluate(expr)
+#infixToPostfix(len(expr), expr)
 #print(expr)
