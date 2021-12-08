46f6b3a39d9e290cfe313aecd3b49f6ddc6fd36d
Ryan Villarreal <Ryan.Villarreal@ttu.edu>
2021-12-08 02:36:18 -0600

    edited code

--- combinational-logic-circuit-analysis.py
+++ combinational-logic-circuit-analysis.py
@@ -1,8 +1,17 @@
 import math as mt
 import string as st
 import re
+from collections import deque
 
-def checkExpress(expr):   #The function ensures user only enters specific characters
+# Data structure to store a binary tree node
+class node:
+    def __init__(self, c):
+        self.data = c
+        self.left = None
+        self.right = None
+
+#The function ensures user only enters specific characters
+def checkExpress(expr):
     n = len(expr)
     #print(n)
     for i in range(n):
@@ -67,12 +76,8 @@ def checkExpress(expr):   #The function ensures user only enters specific charac
         elif(expr[i] == ")"):
             continue
         else:
-            # if(expr[i] != ''):
-            expr = input("Invalid boolean expression. Please enter another: ")
-            checkExpress(expr)
-            # else:
-            #     print("Evaluation success")
-            #     break
+            print("Invalid boolean expression. Please restart program.")
+            exit()
     return expr
 
 #def evaluate(arr, x, y):
@@ -116,66 +121,58 @@ def checkExpress(expr):   #The function ensures user only enters specific charac
 OPERATORS = ['+', '*', "'", '(', ')']  # set of operators
 PRIORITY = {'+':1, '*':1,"'":2} # dictionary having priorities 
 
-def andSym(expr):  #This action is used to add the symbol * for AND logic
+def isOperators(c):
+    return c == "+" or c == "*" or c == "'"
+
+def andSym(expr):  #This function is used to add the symbol * for AND logic
     new_expr = []
     result = ''
 
+    #The for loop is used to search the expression and insert * in between the needed variables
     for i in range(0, len(expr) - 1, 1):
         if(expr[i] not in OPERATORS and expr[i + 1] not in OPERATORS):
             new_expr += expr[i]
             new_expr.append("*")
-            # i += 1
             continue
         elif(expr[i] not in OPERATORS and expr[i + 1] == "("):
-            #new_expr = new_expr[:i + 1] + '*' + new_expr[i + 1:]
             new_expr += expr[i]
             new_expr.append("*")
-            # i += 1
             continue
         elif(expr[i] == ")" and expr[i + 1] not in OPERATORS):
-            #new_expr = new_expr[:i + 1] + '*' + new_expr[i + 1:]
             new_expr += expr[i]
             new_expr.append("*")
-            # i += 1
             continue
         elif(expr[i] not in OPERATORS and expr[i + 1] == "+"):
             new_expr += expr[i]
             new_expr.append("")
-            # i += 1
             continue
         elif(expr[i] not in OPERATORS and expr[i + 1] == "'"):
             new_expr += expr[i]
             new_expr.append("")
-            # i += 1
             continue
         elif(expr[i] == "'" and expr[i + 1] not in OPERATORS):
             new_expr += expr[i]
             new_expr.append("*")
-            # i += 1
             continue
         elif(expr[i] == "'" and expr[i + 1] == "("):
             new_expr += expr[i]
             new_expr.append("*")
-            # i += 1
             continue
         elif(expr[i] == ")" and expr[i + 1] == "+"):
-            #new_expr = new_expr[:i + 1] + '*' + new_expr[i + 1:]
             new_expr += expr[i]
             new_expr.append("")
-            # i += 1
             continue
         elif(expr[i] == ")" and expr[i + 1] == "'"):
-            #new_expr = new_expr[:i + 1] + '*' + new_expr[i + 1:]
             new_expr += expr[i]
             new_expr.append("")
-            # i += 1
             continue
         else:
             new_expr += expr[i]
-            # i += 1
             continue
+
     new_expr.insert(len(new_expr), expr[-1])  #Adds the last string index to stack
     temp = []
+
     while new_expr:  #if stack is not empty, reverse the order of stack
         temp.append(new_expr.pop())
     for item in temp:
@@ -183,27 +180,9 @@ def andSym(expr):  #This action is used to add the symbol * for AND logic
     while new_expr:
         result += new_expr.pop()
 
-    # for i in range(0, n, 2):
-    #     if(expr[i + 1] == "+"):
-    #         i += 1
-    #         continue
-    #     elif(expr[i + 1] == "'"):
-    #         i += 1
-    #         continue
-    #     elif(expr[i + 1] == "("):
-    #         i += 1
-    #         continue
-    #     elif(expr[i + 1] == ")"):
-    #         i += 1
-    #         continue
-    #     else:
-    #         expr = expr[:i] + '*' + expr[i:]
-    #         #res = "".join((expr[:i], '*', expr[i:]))
-    #         i += 1
-    #         continue
     return result
 
-def infix_to_postfix(expr): #input expression
+def infix_to_postfix(expr): #input expression converted to postfix
     stack = [] # initially stack empty
     postfix = '' # initially output empty
 
@@ -226,8 +205,96 @@ def infix_to_postfix(expr): #input expression
         postfix += stack.pop()
     return postfix
 
-expr = input("Enter a boolean expression: ")
-checkExpress(expr)
-infix = andSym(expr)
-print("Infix expression: ", infix)
-print("Postfix expression: ",infix_to_postfix(infix))
+# def add(post):
+#     # If its the end of the expression
+#     if(post == ''):
+#         return ''
+ 
+#     # If the character is an operand
+#     if(post[0] >= 'A' and post[0] <= 'Z'):
+#         return node(post[0]), post[1:]
+#     else:
+#         # Create a node with a[0] as the data and
+#         # both the children set to null
+#         p = node(post[0])
+#         # Build the left sub-tree
+#         p.left,q = add(post[1:])
+#         # Build the right sub-tree
+#         p.right,q = add(q)
+#         return p,q
+
+# # Function to print the postfix expression for the tree
+# def postr(p):
+#     if(p == None):
+#         return
+#     else:
+#         postr(p.left)
+#         postr(p.right)
+#         print(p.data,end = ' ')
+
+# # Function to construct an expression tree from the given postfix expression
+# def construct(post):
+#     root = None
+#     stack = []
+#     while(post):
+#         curr = post.pop()
+#         curr_node = Node(curr)
+#         if(not root):
+#              root = curr_node
+#         if(stack):
+#             parent, side = stack.pop()
+#         if(side == 0):
+#             parent.left = curr_node
+#         else:
+#             parent.right = curr_node
+#         if(not curr.isletter()):
+#             stack.append((curr_node, 0))
+#             stack.append((curr_node, 1))
+#     return root
+
+# def height(root):
+#     if root is None:
+#         return 0
+#     else:
+#         # Compute the height of each subtree
+#         lheight = height(root.left)
+#         rheight = height(root.right)
+ 
+#         # Use the larger one
+#         if lheight > rheight:
+#             return lheight + 1
+#         else:
+#             return rheight + 1
+
+# # Function to  print level order traversal of tree
+# def printLevelOrder(root):
+#     h = height(root)
+#     for i in range(1, h + 1):
+#         printCurrentLevel(root, i)
+#         print("\n\n")
+ 
+ 
+# # Print nodes at a current level
+# def printCurrentLevel(root, level):
+#     if root is None:
+#         return
+#     if level == 1:
+#         print(root.data, end = " ")
+#     elif level > 1:
+#         printCurrentLevel(root.left, level - 1)
+#         printCurrentLevel(root.right, level - 1)
+
+if __name__ == '__main__':
+    expr = input("Enter a boolean expression [please include () wherever possible]: ")
+    checkExpress(expr)
+
+    infix = andSym(expr)
+    print("Infix expression: ", infix)
+    post = infix_to_postfix(infix)
+    print("Postfix expression: ", post)
+    # s,post = add(post)
+    # postr(s)
+
+    #tree = construct(post)
+    # ord = postorder(root)
+    #print("\nTree: ", printLevelOrder(tree))
